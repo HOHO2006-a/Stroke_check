82fbba1d6890896ea1793832095f5f59ea120428
HOHO2006-a <chesshocine@gmail.com>
2026-06-30 10:35:01 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
 
-$host = getenv('MYSQLHOST') ?: '127.0.0.1';
-$dbname = getenv('MYSQLDATABASE') ?: 'stroke_risk_db';
-$username = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: '';  
-$port = getenv('MYSQLPORT') ?: '3306';
+// Les nouvelles informations fournies par votre hébergeur
+$host = getenv('MYSQLHOST') ?: 'strokecheck.howto.rocks'; // Le nom de l'hôte (Host)
+$dbname = getenv('MYSQLDATABASE') ?: 'if0_42303762_strokecheck_db'; // Le nom de la DB en ligne
+$username = getenv('MYSQLUSER') ?: 'if0_42303762'; // Le nom d'utilisateur fourni
+$password = getenv('MYSQLPASSWORD') ?: 'samaxelsky'; // Le mot de passe
+
 
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
